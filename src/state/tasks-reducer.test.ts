7fdb7b2384413cdba6d3ede2174eb3940479d3d9
Den Barca <navigremory.666@gmail.com>
2025-04-26 14:26:59 +0200

    reducer todolist

--- src/state/tasks-reducer.test.ts
+++ src/state/tasks-reducer.test.ts
@@ -1,1 +1,10 @@
-export {};
+test('correct todolist should be removed', () => {
+
+
+    const startState = [
+        {id: "fsdfsdf", title: 'What to learn', filter: 'all'},
+        {id: "number", title: 'What to buy', filter: 'all'}
+    ]
+
+
+})
